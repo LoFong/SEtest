a06168bf6eb0447b3ca221b20c8a93af08ef93bf
LoFong <vivian54628@yahoo.com.tw>
2016-11-18 22:47:27 +0800

    add loginForm.php

---
+++ loginForm.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require("dbconnect.php");
+
+//set the login mark to empty
+$_SESSION['uID'] = "";
+$a=rand(1,10);
+$b=rand(1,10);
+?>
+<h1>Login Form</h1><hr />
+<form method="post" action="loginCheck.php">
+User Name: <input type="text" name="id"><br />
+Password : <input type="password" name="pwd"><br />
+How much is <?php echo "$a + $b";?>: <input type="text" name="answer"><br />
+<input type="hidden" name="answer0" value="<?php echo $a+$b;?>">
+<input type="submit">
+</form>
